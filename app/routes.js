8b38e59d1525f07133ae23fcb2b10f8a18508804
darraghmckeogh <darragh.mckeogh@paconsulting.com>
2023-11-13 09:25:42 +0000

    round 4

--- app/routes.js
+++ app/routes.js
@@ -72,7 +72,7 @@ router.post('/already-provided-all-answer', function (req, res) {
   
   })
 
-  */
+  
 
   // V1-5
 
@@ -131,6 +131,94 @@ router.post('/already-provided-all-answer', function (req, res) {
       res.redirect('/v1-5/card-payment')
     } else {
       res.redirect('/v1-5/confirmation')
+    }
+  
+  })
+
+  */
+
+
+  // V2
+
+  router.post('/single-or-multiple-files-answer', function (req, res) {
+
+    var connectedBuildings = req.session.data['single-or-multiple-files']
+    if (connectedBuildings == "multiple") {
+      res.redirect('/v2/upload-scr')
+    } else {
+      res.redirect('/v2/upload-all')
+    }
+  
+  })
+
+
+router.post('/already-provided-res-answer', function (req, res) {
+
+    var resDoc = req.session.data['already-provided-res']
+    if (resDoc == "yes") {
+      res.redirect('/v2/already-provided-res-explanation')
+    } else {
+      res.redirect('/v2/upload-mor')
+    }
+  
+  })
+
+
+router.post('/already-provided-mor-answer', function (req, res) {
+
+    var morDoc = req.session.data['already-provided-mor']
+    if (morDoc == "yes") {
+      res.redirect('/v2/already-provided-mor-explanation')
+    } else {
+      res.redirect('/v2/invoice')
+    }
+  
+  })
+
+
+router.post('/already-provided-all-answer', function (req, res) {
+
+    var allDocs = req.session.data['already-provided-all']
+    if (allDocs == "yes") {
+      res.redirect('/v2/already-provided-all-explanation')
+    } else {
+      res.redirect('/v2/invoice')
+    }
+  
+  })
+
+
+  router.post('/payment-method-answer', function (req, res) {
+
+    var paymentMethod = req.session.data['payment-method']
+    if (paymentMethod == "card") {
+      res.redirect('/v2/card-payment')
+    } else {
+      res.redirect('/v2/confirmation')
+    }
+  
+  })
+
+
+  router.post('/subject-to-licensing-answer', function (req, res) {
+
+    var subjectToLicensing = req.session.data['subject-to-licensing']
+    if (subjectToLicensing == "yes") {
+      res.redirect('/v2/is-it-licensed')
+    } else {
+      res.redirect('/v2/enforcement-action')
+    }
+  
+  })
+
+
+  router.post('/enforcement-action-answer', function (req, res) {
+
+    var enforcementAction = req.session.data['enforcement-action']
+    if (enforcementAction == "No") {
+      res.redirect('/v2/check-answers')
+    } else {
+      res.redirect('/v2/enforcement-action-details')
     }
   
   })
